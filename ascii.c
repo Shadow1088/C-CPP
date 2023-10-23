430537dfd010abe1941b80fcb7b55bd0197b0096
ZZ <145645832+Shadow1088@users.noreply.github.com>
2023-10-23 12:24:29 +0200

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,25 @@
+# THIS CODE PRINTS OUT ASCII SYMBOLS FROM 32 TO 127, FIRST USE OF FOR LOOP IN C
+
+
+#include <stdio.h>
+
+#define SIZE 128
+#define COLUMN 5
+#define START 0
+
+
+
+int main() {
+
+   int symbol;
+   int count = 0;
+
+   for(symbol=32; symbol<SIZE; symbol++){
+      printf("%4d %c", symbol, symbol);
+      count++;
+      if (count % COLUMN == 0) {
+      printf("\n");
+      }
+   }
+   return 0;
+}
