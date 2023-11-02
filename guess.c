c6ab5bb377a9eb4d89fe97257d32fb7fc48904d5
ZZ <145645832+Shadow1088@users.noreply.github.com>
2023-11-02 13:47:36 +0100

    Create guess.c

---
+++ guess.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 10
+
+int main(){
+
+    srand((unsigned)time(NULL));
+    int random0 = rand()%(MAX + 1);
+    int guess;
+    int countOfChar;
+
+    printf("Write the number: ");
+    do{
+        scanf("%d", &guess);
+        
+        countOfChar = 0;
+        while (getchar() != '\n'){
+        countOfChar++;
+        }   
+        
+
+        if(countOfChar > 1 || guess > MAX){
+            printf("Wrong input!\n");
+        }else if(guess < random0){
+            printf("Too low\n");
+        }else if (guess > random0){
+            printf("Too high!\n");
+        }else{
+            printf("You got it!\n");
+        }
+        
+    }while (guess != random0);
+    return 0;
+
+}
